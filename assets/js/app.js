92bac6c607549edccef7d53da5045c8c81de8666
manarkbyeh <manar.kbyeh@student.kdg.be>
2018-01-18 18:16:23 +0100

    overzicht page verdeler stijlen met grotre errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 "use strick";
 $(document).ready(function() {
     $('.carousel').carousel('pause');
+
+    $('#manar-toggle-list').click(function() {
+        $('#manar-list-toggle-selector').toggle();
+
+        $(this).toggleClass('manarrotatison');
+    })
+
 });
