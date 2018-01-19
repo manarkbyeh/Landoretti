468705bb92f2206356ca52c975e0c62fcc15143a
manarkbyeh <manar.kbyeh@student.kdg.be>
2018-01-19 16:05:27 +0100

    verder stylen van de newaction

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
         $('#manar-list-toggle-selector').toggle();
 
         $(this).toggleClass('manarrotatison');
-    })
+    });
 
+    $('.input-group').click(function(event) {
+        event.stopPropagation();
+        $('.input-group').removeClass('input-group-is-fouses')
+        $(this).addClass('input-group-is-fouses')
+    })
+    $(document).click(function() {
+        $('.input-group').removeClass('input-group-is-fouses')
+    })
 });
